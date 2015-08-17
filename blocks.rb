2e101b676abcdbecb07d9542d52a9875cda939fe
JoyLynn <joylynnious@gmail.com>
2015-08-17 20:23:26 +0300

    added some changes

--- blocks.rb
+++ blocks.rb
@@ -1,9 +1,18 @@
 #!/usr/bin/ruby
 
-def sample
-   puts "You are in the club"
-   yield
-   puts "You are again back to the club"
-   yield
+# def sample
+#    puts "You are in the club"
+#    yield
+#    puts "You are again back to the club"
+#    yield
+# end
+# test {puts "You are in the box"}
+
+#!/usr/bin/ruby
+
+def test
+   yield 5
+   puts "You are in the method test"
+   yield 100
 end
-test {puts "You are in the box"}
+test {|i| puts "You are in the block #{i}"}
