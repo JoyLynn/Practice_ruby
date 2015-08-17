c80f17f79556aa0885126f4ecf4a291b210ca166
JoyLynn <joylynnious@gmail.com>
2015-08-17 20:12:29 +0300

    added some changes

--- methods.rb
+++ methods.rb
@@ -17,3 +17,14 @@ return i, j, k
 end
 var = test
 puts var
+
+#!/usr/bin/ruby
+
+def sample (*test)
+   puts "The number of words is #{test.length}"
+   for i in 0...test.length
+      puts "The words are #{test[i]}"
+   end
+end
+sample "Clara", "5", "B"
+sample "Mad", "28", "L", "BMV"
